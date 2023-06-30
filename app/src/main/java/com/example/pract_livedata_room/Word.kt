58675dfa470c060856dc538469c644e320cdd1f5
Егор Лапковский <egorka004@inbox.ru>
2023-06-30 05:11:19 -0700

    Initial commit

---
+++ app/src/main/java/com/example/pract_livedata_room/Word.kt
@@ -0,0 +1,12 @@
+package com.example.pract_livedata_room
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+class Word(
+    @PrimaryKey(autoGenerate = true) val id:Int,
+    @ColumnInfo(name = "word") val word:String
+    )
+
